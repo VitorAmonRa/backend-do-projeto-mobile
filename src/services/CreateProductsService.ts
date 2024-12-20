5baf6a812f39d3dfbe31f939b2992dfa9775d82a
Vitor Amon-Ra Fernandes <vitoramon-ra@hotmail.com>
2024-12-20 11:19:36 -0300

    .

--- src/services/CreateProductsService.ts
+++ src/services/CreateProductsService.ts
@@ -2,7 +2,6 @@ import PrismaClient from "../prisma";
 import { DataProps } from "../controllers/CreateProductsController";
 import prisma from "../prisma";
 
-
 const calculateDiscount = (expiryDate: string, originalPrice: string): string => {
     const today = new Date();
     const [day, month, year] = expiryDate.split('/');
@@ -17,32 +16,37 @@ const calculateDiscount = (expiryDate: string, originalPrice: string): string =>
     console.log(`Day Difference: ${dayDifference}`);
     
     const priceNumber = parseFloat(originalPrice);
+    console.log(`Original Price: ${originalPrice}`);
+    console.log(`Parsed Price: ${priceNumber}`);
+    
     if (isNaN(priceNumber)) {
         throw new Error("Invalid price format");
     }
 
-    if (dayDifference <= 7) {
+    if (dayDifference <= 15) {
         return (priceNumber * 0.9).toFixed(2); // 10% de desconto
     }
     return originalPrice;
 };
-  
-  class CreateProductsService {
-      async execute({ name, validate, amount, price }: DataProps) {
-          const discountedPrice = calculateDiscount(validate, price);
-  
-          const product = await prisma.products.create({
-              data: {
-                  name,
-                  validate,
-                  amount,
-                  price,
-                  discountedPrice,
-              }
-          });
-  
-          return product;
-      }
-  }
-  
-  export { CreateProductsService };
+
+class CreateProductsService {
+    async execute({ name, validate, amount, price }: DataProps) {
+        const discountedPrice = calculateDiscount(validate, price);
+
+        const product = await prisma.products.create({
+            data: {
+                name,
+                validate,
+                amount,
+                price,
+            }
+        });
+
+        return {
+            ...product,
+            discountedPrice,
+        };
+    }
+}
+
+export { CreateProductsService };
