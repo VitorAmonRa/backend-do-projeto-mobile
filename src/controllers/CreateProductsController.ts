93a6a48abb195d82a19c79f2f3c934619b93db88
Vitor Amon-Ra Fernandes <vitoramon-ra@hotmail.com>
2024-12-20 01:55:59 -0300

    .

--- src/controllers/CreateProductsController.ts
+++ src/controllers/CreateProductsController.ts
@@ -12,14 +12,12 @@ class CreateProductsController {
     async handle(request: FastifyRequest, reply: FastifyReply) {
         const { name, validate, amount, price } = request.body as DataProps;
 
-        console.log("Dados recebidos no backend:", { name, validate, amount, price });
-
         const create = new CreateProductsService();
+
         try {
             const products = await create.execute({ name, validate, amount, price });
             reply.send(products);
         } catch (error) {
-            console.error("Erro ao criar produto:", error);
             reply.status(400).send({ error: "Erro ao criar produto" });
         }
     }
--- src/services/CreateProductsService.ts
+++ src/services/CreateProductsService.ts
@@ -1,22 +1,15 @@
 import PrismaClient from "../prisma";
 import { DataProps } from "../controllers/CreateProductsController";
 
-// Função para converter a data para o formato ISO
-function convertDateToISO(dateString: string): string {
-    const [day, month, year] = dateString.split('/');
-    return `20${year}-${month}-${day}`;
-}
 
 class CreateProductsService {
     async execute({ name, validate, amount, price }: DataProps) {
-        // Converte a data de validade para o formato ISO
-        const formattedDate = convertDateToISO(validate);
 
         // Criação do produto no banco de dados
         const product = await PrismaClient.products.create({
             data: {
                 name,
-                validate: formattedDate,  // Data convertida para o formato ISO
+                validate,
                 amount,
                 price,
             }
