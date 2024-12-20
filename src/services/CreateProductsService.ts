c766bab45f048c9df365cb1dc9360b78ef05dee3
Vitor Amon-Ra Fernandes <vitoramon-ra@hotmail.com>
2024-12-20 03:45:44 -0300

    .

--- src/services/CreateProductsService.ts
+++ src/services/CreateProductsService.ts
@@ -7,14 +7,25 @@ const calculateDiscount = (expiryDate: string, originalPrice: string): string =>
     const today = new Date();
     const [day, month, year] = expiryDate.split('/');
     const productExpiry = new Date(Number(`20${year}`), Number(month) - 1, Number(day));
+    
+    console.log(`Today: ${today}`);
+    console.log(`Expiry Date: ${productExpiry}`);
+    
     const timeDifference = productExpiry.getTime() - today.getTime();
     const dayDifference = timeDifference / (1000 * 3600 * 24);
-  
-    if (dayDifference <= 15) {
-      return (parseFloat(originalPrice) * 0.9).toFixed(2); // 10% de desconto
+    
+    console.log(`Day Difference: ${dayDifference}`);
+    
+    const priceNumber = parseFloat(originalPrice);
+    if (isNaN(priceNumber)) {
+        throw new Error("Invalid price format");
     }
-    return originalPrice
-  };
+
+    if (dayDifference <= 15) {
+        return (priceNumber * 0.9).toFixed(2); // 10% de desconto
+    }
+    return originalPrice;
+};
   
   class CreateProductsService {
       async execute({ name, validate, amount, price }: DataProps) {
