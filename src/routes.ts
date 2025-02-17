3bc52fc5303a8d2a5a4d4c4da9f6291907162e23
Vitor Amon-Ra Fernandes <vitoramon-ra@hotmail.com>
2025-02-17 18:10:20 -0300

    .

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,6 @@ import { CreateProductsController } from './controllers/CreateProductsController
 import { ListProductsController } from './controllers/ListProductsController'
 import { DeleteProductsController } from './controllers/DeleteProductsController'
 import { AuthUserController } from "./controllers/AuthUserController";
-import { PriceMarkdownController } from "./controllers/PriceMarkdownController";
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
     
@@ -12,7 +11,6 @@ export async function routes(fastify: FastifyInstance, options: FastifyPluginOpt
 
         reply.send({ ok: true })
     })
-
     fastify.get("/products", async (request: FastifyRequest, reply: FastifyReply) => {
         return new ListProductsController().handle( request, reply)
     })
@@ -24,8 +22,5 @@ export async function routes(fastify: FastifyInstance, options: FastifyPluginOpt
     })
     fastify.post("/login", async (request: FastifyRequest, reply: FastifyReply) => {
         return new AuthUserController().login( request, reply)
-    })
-    fastify.get("/product/markdown", async (request: FastifyRequest, reply: FastifyReply) => {
-        return new PriceMarkdownController().handle( request, reply)
     })
 } 
